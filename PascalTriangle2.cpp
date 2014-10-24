0e0e6ad5d61e72b0c0e605286e77e7cfed8a8255
haoya5509 <haoyunsheng1201@gmail.com>
2014-10-24 08:49:59 +0800

    modify Traiangle2.cpp use space(K)

--- PascalTriangle2.cpp
+++ PascalTriangle2.cpp
@@ -16,20 +16,18 @@ Time 12:55 2014/10/24
 using namespace std;
 class Solution{
 public:
-    vector<int> getRow(int rowIndex){
-        vector<int> res(1,1);
-        if(rowIndex<0) return res;
-        for(int i=1;i<=rowIndex;i++){
-            vector<int> tmp(1,1);
-            for(int j=1;j<i;j++){
-                tmp.push_back(res[j-1]+res[j]);
-            }
-            tmp.push_back(1);
-            res = tmp;
-        }
-        return res;
-    }
+	vector<int> getRow(int rowIndex){
+		vector<int> res(1,1);
+		for(int i=1;i<=rowIndex;i++){
+			for(int j=i-1;j>0;j--){
+				res[j] = res[j]+res[j-1];
+			}
+			res.push_back(1);
+		}
+		return res;
+	}
 };
+
 int main(){
     Solution solution;
     vector<int> cnt = solution.getRow(4);
