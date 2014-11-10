9cccd02319b546638456afa3f86cbebd079cfe34
haoya <haoyunsheng1201@gmail.com>
2014-11-10 18:31:55 +0800

    LinkListCycle2.cpp

--- LinkedListCycleII.cpp
+++ LinkedListCycleII.cpp
@@ -1,11 +1,34 @@
+/**
+Linked List Cycle II Total Accepted: 27777 Total Submissions: 90267 My Submissions Question Solution 
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space?
+
+Author: haoya5509
+Time: 18:31pm 2014/11/10
+*/
 #include<iostream>
 using namespace std;
 ListNode *detectCycle(ListNode *head) {
 	if(!head) return NULL;
 	if(!hasCycle(head))
 		return NULL;
-	ListNode *p=head,*q=head;
-	
+	ListNode *p=head,*q=head->next;
+	while(p!=q){
+		p=p->next;
+		q=q->next->next;
+	}
+	ListNode *t=head;
+	while(true){
+		p=p->next;
+		if(p==t) return t;	//!!!self-circle
+		while(p!=q){
+			if(p==t) return t;
+			p=p->next;
+		}
+		t=t->next;
+	}
 }
 bool hasCycle(ListNode *head){
 	if(!head) return false;
